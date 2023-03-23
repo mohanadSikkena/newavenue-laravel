8e8471f66f3974e2ab1e294e386fefb7cfbd5fe9
mohanad sikkena <mo.sal5548@gmail.com>
2023-03-23 15:54:40 +0200

    commit

---
+++ app/Models/Location.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+use App\Models\Property;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Location extends Model
+{
+    use HasFactory;
+
+    public function primary(){
+        return $this->hasMany(PrimaryProperty::class)
+        ->select(['id','name','delivery_date','price','min_space','max_space'])->with('images:image,primary_property_id');
+      }
+}
