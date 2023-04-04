a9b14c739823482bf2bac22e4024bb43f14b4364
mohanad sikkena <mo.sal5548@gmail.com>
2023-04-04 15:47:25 +0200

    testing

--- config/filesystems.php
+++ config/filesystems.php
@@ -39,7 +39,7 @@ return [
         'public' => [
             'driver' => 'local',
             'root' => storage_path('app/images'),
-            'url' => env('APP_URL'),
+            'url' => env('APP_URL').'/storage',
             'visibility' => 'public',
             'throw' => false,
         ],
@@ -70,7 +70,7 @@ return [
     */
 
     'links' => [
-        public_path('public_html/storage') => storage_path('app/images'),
+        public_path('images') => storage_path('app/images'),
     ],
 
 ];
