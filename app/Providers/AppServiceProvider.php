f6cb099df6f216ccd23815afd6d7b8a3607bb905
mohanad sikkena <mo.sal5548@gmail.com>
2023-04-04 15:32:02 +0200

    test

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -15,8 +15,8 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app->bind('path.public', function() {
-            return realpath(base_path().'/../public_html');
+        $this->app->bind('path.public', function(){
+            return base_path().'/public_html';
         });
 
     }
