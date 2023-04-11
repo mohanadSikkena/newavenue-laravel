390ac0f9ea07969a8f0b935624284847e213a1d4
mohanad sikkena <mo.sal5548@gmail.com>
2023-04-11 16:03:59 +0200

    commit

--- app/Console/cron.sh
+++ app/Console/cron.sh
@@ -1,1 +1,1 @@
-cd /home/u630246912/domains/newavenue-investment.com && php artisan schedule:run >> /dev/null 2>&1
+cd /home/u630246912/domains/newavenue-investment.com/public_html && php artisan schedule:run >> /dev/null 2>&1
