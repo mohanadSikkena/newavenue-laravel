2f387eb6b53fb1eb17307bc3d60e11b8c713b1b7
mohanad sikkena <mo.sal5548@gmail.com>
2023-04-11 16:03:40 +0200

    last cron

--- app/Console/cron.sh
+++ app/Console/cron.sh
@@ -1,1 +1,1 @@
-cd /home/u285707107/domains/newavenue-investment.com && php artisan schedule:run >> /dev/null 2>&1
+cd /home/u630246912/domains/newavenue-investment.com && php artisan schedule:run >> /dev/null 2>&1
